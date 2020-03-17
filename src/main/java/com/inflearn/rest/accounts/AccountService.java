84e82f03d0ea635b29764f90fb6e73c1d907d4a6
huisam <huisam@naver.com>
2020-03-17 22:47:24 +0900

    feat(oauth): Account를 도입해서 인증하는 Service 구현

---
+++ src/main/java/com/inflearn/rest/accounts/AccountService.java
@@ -0,0 +1,34 @@
+package com.inflearn.rest.accounts;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class AccountService implements UserDetailsService {
+
+    private AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toSet());
+    }
+}
---
+++ src/test/java/com/inflearn/rest/accounts/AccountServiceTest.java
@@ -0,0 +1,56 @@
+package com.inflearn.rest.accounts;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AccountServiceTest {
+    @Autowired
+    private AccountService service;
+
+    @Autowired
+    private AccountRepository repository;
+
+    @Test
+    @DisplayName("인증된 유저가 주어졌을 때 제대로 인증 서비스에 저장되는지 테스트")
+    void findByUserName() {
+        /* given */
+        final String email = "huisam@naver.com";
+        final String password = "hwijin";
+        Account account = Account.builder()
+                .email(email)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.repository.save(account);
+
+        /* when */
+        final UserDetails userDetails = service.loadUserByUsername(email);
+
+        /* then */
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    @DisplayName("인증되지 않는 유저가 주어졌을 때 예외를 던지는지 테스트")
+    void findByUserNameFail() {
+        /* given */
+        final String email = "huisam@naver.com";
+
+        /* when, then */
+        final UsernameNotFoundException usernameNotFoundException = assertThrows(
+                UsernameNotFoundException.class, () -> service.loadUserByUsername(email));
+        assertThat(usernameNotFoundException.getMessage()).contains(email);
+    }
+}
